ce55db5ff8681c75f38aa83dee2c05afd3a9c2d3
Nazar Leshchyshyn <lesh1.nazar@gmail.com>
2021-03-03 15:48:08 +0200

    smallFix

--- src/views/Homepage/Homepage.jsx
+++ src/views/Homepage/Homepage.jsx
@@ -1,6 +1,6 @@
-import { useEffect, useState, useMemo } from "react";
-import { Products, Filter } from "../../components";
-import fetchProducts from "../../services/fetch";
+import { useEffect, useState, useMemo } from 'react';
+import { Products, Filter } from '../../components';
+import fetchProducts from '../../services/fetch';
 
 const Homepage = () => {
   const [products, setProducts] = useState([]);
@@ -15,8 +15,8 @@ const Homepage = () => {
   };
 
   const filteredProducts = useMemo(
-    () => products.filter((p) => p.name.toLowerCase().includes(filterState)),
-    [filterState, products]
+    () => products.filter(p => p.name.includes(filterState)),
+    [filterState, products],
   );
 
   return (
